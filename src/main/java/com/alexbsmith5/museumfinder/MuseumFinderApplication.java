59e1aa0b400cee3a84dcb518e303cee5bc9528e2
alexbsmith5 <alexbsmith52@gmail.com>
2026-08-10 14:31:00 -0400

    Initial commit

---
+++ src/main/java/com/alexbsmith5/museumfinder/MuseumFinderApplication.java
@@ -0,0 +1,13 @@
+package com.alexbsmith5.museumfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseumFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexbsmith5/museumfinder/MuseumFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexbsmith5.museumfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
