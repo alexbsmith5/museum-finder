9507f042761db2bbe669a80b6fb7bd55bf9d9431
alexbsmith5 <alexbsmith52@gmail.com>
2026-08-11 12:48:29 -0400

    Remove unnecessary dependencies

--- src/test/java/com/alexbsmith5/museumfinder/MuseumFinderApplicationTests.java
+++ src/test/java/com/alexbsmith5/museumfinder/MuseumFinderApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MuseumFinderApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
