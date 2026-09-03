ab8daf25789de408686a9355f92c50f739697d56
alexbsmith5 <alexbsmith52@gmail.com>
2026-09-03 16:54:23 -0400

    Display /list api on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,28 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 function App() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/list?itemId=Q5593")
+      .then((response) => {
+        setItems(response.data.results.bindings);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
   return (
     <>
       <h1>Hello World</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.item.value}>
+            <a href={item.item.value}>{item.itemLabel.value}</a>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
