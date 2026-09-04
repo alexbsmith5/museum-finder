3bc487dcde140f1c5cd3903b51ac067012f61d61
alexbsmith5 <alexbsmith52@gmail.com>
2026-09-04 15:37:42 -0400

    Add dark/light mode toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { ThemeToggle } from "@/components/ThemeToggle";
 import SearchBar from "./components/SearchBar";
 import ListWorks from "./components/ListWorks";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeToggle />
       <SearchBar onChange={setSearchTerm} />
       <ListWorks searchTerm={searchTerm} />
-    </>
+    </ThemeProvider>
   );
 }
 
