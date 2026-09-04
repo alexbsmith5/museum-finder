44ec7056482a7aedc0784ed7a55ca8609b72436b
alexbsmith5 <alexbsmith52@gmail.com>
2026-09-04 15:20:42 -0400

    Install shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,21 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
